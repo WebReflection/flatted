9dbe2d4be9f9da1d429c10d5cd0f6fc1e866b666
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-09-18 19:51:40 +0200

    trying to squeeze stringify in too

--- cjs/index.js
+++ cjs/index.js
@@ -23,7 +23,7 @@ var Flatted = (function (Primitive, primitive) {
     parse: function parse(text, reviver) {
       var input = JSON.parse(text, Primitives).map(primitives);
       var value = input[0];
-      var $ = reviver || function (k, v) { return v; };
+      var $ = reviver || noop;
       return $(
         '',
         typeof value === 'object' && value ?
@@ -32,27 +32,29 @@ var Flatted = (function (Primitive, primitive) {
       );
     },
 
-    stringify: function stringify(value) {
+    stringify: function stringify(value, replacer, space) {
       for (var
         firstRun,
         known = new Map,
         input = [],
         output = [],
-        i = +set(known, input, value),
+        $ = replacer || noop,
+        i = +set(known, input, $('', value)),
         replace = function (key, value) {
           if (firstRun) return (firstRun = !firstRun), value;
-          switch (typeof value) {
+          var after = $('', value);
+          switch (typeof after) {
             case 'object':
-              if (value === null) return value;
+              if (after === null) return after;
             case primitive:
-              return known.get(value) || set(known, input, value);
+              return known.get(after) || set(known, input, after);
           }
-          return value;
+          return after;
         };
         i < input.length; i++
       ) {
         firstRun = true;
-        output[i] = JSON.stringify(input[i], replace);
+        output[i] = JSON.stringify(input[i], replace, space);
       }
       return '[' + output.join(',') + ']';
     }
@@ -60,6 +62,10 @@ var Flatted = (function (Primitive, primitive) {
   };
 
   return Flatted;
+
+  function noop(key, value) {
+    return value;
+  }
 
   function revive(input, parsed, output, $) {
     return Object.keys(output).reduce(
