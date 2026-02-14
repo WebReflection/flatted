7326401f34420c80648e967945b2ff9a242d857f
Harald Fielker <harald.fielker@gmail.com>
2026-02-14 16:02:28 +0100

    added --sort keys

--- golang/main.go
+++ golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -9,33 +10,80 @@ import (
 	"github.com/WebReflection/flatted/golang/pkg/flatted"
 )
 
-func main() {
-	stat, _ := os.Stdin.Stat()
-	if (stat.Mode() & os.ModeCharDevice) != 0 {
-		fmt.Fprintln(os.Stderr, "Usage: echo <circular-json> | flatted")
-		os.Exit(1)
-	}
-
-	input, err := io.ReadAll(os.Stdin)
+// flatten reads standard JSON from r and writes flatted JSON to w.
+func flatten(r io.Reader, w io.Writer) error {
+	input, err := io.ReadAll(r)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
-		os.Exit(1)
+		return err
 	}
+	var data any
+	if err := json.Unmarshal(input, &data); err != nil {
+		return fmt.Errorf("invalid JSON input: %w", err)
+	}
+	s, err := flatted.Stringify(data, nil, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(w, s)
+	return nil
+}
 
-	// Parse the flatted input
+// unflatten reads flatted JSON from r and writes standard JSON to w.
+func unflatten(r io.Reader, w io.Writer) error {
+	input, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
 	parsed, err := flatted.Parse(string(input), nil)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error parsing flatted data: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("invalid flatted input: %w", err)
 	}
-
-	// Convert to standard JSON for output.
-	// Note: If the revived object is still circular, json.Marshal will error.
 	output, err := json.MarshalIndent(parsed, "", "  ")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error marshaling to JSON: %v\n", err)
-		os.Exit(1)
+		return err
+	}
+	fmt.Fprintln(w, string(output))
+	return nil
+}
+
+func main() {
+	var decompress bool
+	flag.BoolVar(&decompress, "d", false, "decompress (unflatten)")
+	flag.BoolVar(&decompress, "decompress", false, "decompress (unflatten)")
+	flag.BoolVar(&decompress, "unflatten", false, "decompress (unflatten)")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTION]... [FILE]\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Flatten or unflatten circular JSON structures.")
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "Options:")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "")
+		fmt.Fprintln(os.Stderr, "If no FILE is provided, or if FILE is -, read from standard input.")
 	}
 
-	fmt.Println(string(output))
+	flag.Parse()
+
+	var r io.Reader = os.Stdin
+	if flag.NArg() > 0 && flag.Arg(0) != "-" {
+		f, err := os.Open(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		r = f
+	}
+
+	var err error
+	if decompress {
+		err = unflatten(r, os.Stdout)
+	} else {
+		err = flatten(r, os.Stdout)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(1)
+	}
 }
